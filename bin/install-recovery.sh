ff40f2376944f17bf689593e9da8537931778796
minz1 <emerytang@gmail.com>
2017-06-10 14:16:21 -0400

    j7ltespr: update to J700PVPS1AQD1

--- bin/install-recovery.sh
+++ bin/install-recovery.sh
@@ -1,6 +1,46 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:12535084:dfbe9def034c6f09563ec0e074ba87ccfda4b10a; then
-  applypatch EMMC:/dev/block/bootdevice/by-name/boot:11154728:a87f012637a624cad49d7bfb64cae6c3f708ccb3 EMMC:/dev/block/bootdevice/by-name/recovery dfbe9def034c6f09563ec0e074ba87ccfda4b10a 12535084 a87f012637a624cad49d7bfb64cae6c3f708ccb3:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/bootdevice/by-name/recovery:12742928:774c088ca8298b69aadf672b8318ea066387f6e1; then
+  applypatch EMMC:/dev/block/bootdevice/by-name/boot:11362576:ca1298b7bea1566bc60efcbfb86a1685f0940cae EMMC:/dev/block/bootdevice/by-name/recovery 774c088ca8298b69aadf672b8318ea066387f6e1 12742928 ca1298b7bea1566bc60efcbfb86a1685f0940cae:/system/recovery-from-boot.p || echo 454 > /cache/fota/fota.status
 else
   log -t recovery "Recovery image already installed"
+fi
+
+if [ -e /cache/recovery/command ] ; then
+  PACKAGE_PATH=""
+  SEARCH_COMMAND="--update_package"
+  PATH_POS=16
+  if [ -e '/system/bin/grep' ] ; then
+    PACKAGE_PATH=`cat /cache/recovery/command | grep 'update_package'`
+    PACKAGE_ORG_PATH=`cat /cache/recovery/command | grep 'update_org_package'`
+    if [ "$PACKAGE_ORG_PATH" != "" ] ; then
+      PACKAGE_PATH=$PACKAGE_ORG_PATH
+      SEARCH_COMMAND="--update_org_package"
+      PATH_POS=20
+    fi
+    if [ -e /cache/recovery/saved" ] ; then
+      rm -rf /cache/recovery/saved
+    fi
+
+    if [ -e /data/.recovery/saved" ] ; then
+      rm -rf /data/.recovery/saved
+    fi
+  fi
+  if [ "$PACKAGE_PATH" != "" ] ; then
+    for PACKAGE_LINE in $PACKAGE_PATH
+    do
+      if [ ${PACKAGE_LINE:0:$PATH_POS} == $SEARCH_COMMAND ] ; then
+        break
+      fi
+    done
+    let PATH_POS+=1
+    PACKAGE_PATH=${PACKAGE_LINE:$PATH_POS}
+    if [ "$PACKAGE_PATH" != "" ] ; then
+      rm $PACKAGE_PATH
+    fi
+  fi
+  if [ ${PACKAGE_PATH:0:5} == "/data" ] ; then
+    echo $PACKAGE_PATH > /cache/fota/fota_path_command
+    chown system:system /cache/fota/fota_path_command
+  fi
+  rm /cache/recovery/command
 fi
